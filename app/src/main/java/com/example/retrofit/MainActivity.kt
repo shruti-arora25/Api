ee876a86c5a80a5dcd6836a2500bdaffb1d6a0d8
Shruti <shrutiarora2507@gmail.com>
2024-03-29 09:25:54 +0530

    Comited

--- app/src/main/java/com/example/retrofit/MainActivity.kt
+++ app/src/main/java/com/example/retrofit/MainActivity.kt
@@ -4,14 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.retrofit.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,43 +20,69 @@ class MainActivity : AppCompatActivity() {
     private lateinit var bind: ActivityMainBinding
     private lateinit var list: ArrayList<Data>
     lateinit var adapterC: RecyclerAdapter
+    private lateinit var retro: ApiInterface
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        bind = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(bind.root)
         Log.d("Tag", "Country")
 
 
-        bind = ActivityMainBinding.inflate(layoutInflater)
         list = ArrayList()
 
         adapterC = RecyclerAdapter(list, this@MainActivity)
-        bind.recycle.adapter=adapterC
+        bind.recycle.adapter = adapterC
 
-          val layoutManagerC = LinearLayoutManager(this)
+        val layoutManagerC = LinearLayoutManager(this)
         bind.recycle.layoutManager = layoutManagerC
 
 
 
 
-        method()
 
-        Log.d("Tag", "Country")
+        retro = Retrofit.Builder().baseUrl("https://sibyl.zealstrat.com/api/v1/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build().create(ApiInterface::class.java)
+
+
+        lifecycleScope.launch {
+            suspendfunc()
+        }
+
+
+        // method()
+
+
+    }
+
+    private suspend fun suspendfunc() {
+        var response =
+            retro.getfunData().body()
+
+
+        for (myData in response?.data!!) {
+            list.add(myData)
+
+        }
+
+
+        adapterC = RecyclerAdapter(list, this@MainActivity)
+        adapterC.notifyDataSetChanged()
+
+
+
+        bind.recycle.adapter = adapterC
 
 
     }
 
 
     fun method() {
-        val retro = Retrofit.Builder().baseUrl("https://sibyl.zealstrat.com/api/v1/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build().create(ApiInterface::class.java)
 
 
         val data: Call<dataclass> = retro.getData()
-
-
-
 
         data.enqueue(object : Callback<dataclass> {
 
--- app/src/main/java/com/example/retrofit/RecyclerAdapter.kt
+++ app/src/main/java/com/example/retrofit/RecyclerAdapter.kt
@@ -6,7 +6,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.retrofit.databinding.ListItemBinding
 
-class RecyclerAdapter(private val List: ArrayList<String>, context: Context) :
+class RecyclerAdapter(private val List: ArrayList<Data>, context: Context) :
     RecyclerView.Adapter<RecyclerAdapter.MyViewHolder>() {
 
 
@@ -22,7 +22,7 @@ class RecyclerAdapter(private val List: ArrayList<String>, context: Context) :
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val currentItem = List[position]
         holder.apply {
-            CountryName.text = currentItem
+            CountryName.text = currentItem.toString()
         }
 
     }
